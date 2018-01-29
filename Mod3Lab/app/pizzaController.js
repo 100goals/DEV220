b83a57aed48798941c900764ad2750bea0845028
Mathew Somers <100Goals@gmail.com>
2018-01-29 14:45:37 -0500

    Update

--- Mod3Lab/app/pizzaController.js
+++ Mod3Lab/app/pizzaController.js
@@ -3,7 +3,15 @@ app.controller('pizzaController', [
     function ($scope) {
         $scope.model = {
             title: 'Pizza Builder',
-            availableToppings: ['Cheese', 'Pepperoni', 'Bacon', 'Pineapple', 'Sausage', 'Ham', 'Chicken', 'Mushrooms', 'Onion', 'Olives', 'Green Peppers']
+            
+            availableToppings: ['Cheese', 'Pepperoni', 'Bacon', 'Pineapple', 'Sausage', 'Ham', 'Chicken', 'Mushrooms', 'Onion', 'Olives', 'Green Peppers'],
+            
+            toppings: []
         };
+
+        $scope.addTopping = function(topping){
+            $scope.model.toppings.push(topping);
+            $scope.model.search = null;
+        }
     }
 ]);
